169e67b257d77231e581678732440afe876bef7c
WP septa <wpxchou@gmail.com>
2026-07-30 16:30:40 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("frontend"));
+
+app.get("/api", (req, res) => {
+    res.json({
+        status: "online",
+        message: "DriveCloud API Berjalan"
+    });
+});
+
+// Route
+const uploadRoute = require("./routes/upload");
+app.use("/api/upload", uploadRoute);
+
+app.listen(PORT, () => {
+    console.log(`DriveCloud berjalan di http://localhost:${PORT}`);
+});
