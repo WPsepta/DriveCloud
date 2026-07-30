52906c3a4ee55d75ff9224903546aeed61152853
WP septa <wpxchou@gmail.com>
2026-07-30 21:59:09 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,34 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-const multer = require("multer");
-const { google } = require("googleapis");
-const { Readable } = require("stream");
-
-require("dotenv").config();
-
-const app = express();
-
-app.use(cors());
-
-const upload = multer({
-    storage: multer.memoryStorage()
-});
-
-const auth = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-);
-
-auth.setCredentials({
-    refresh_token: process.env.REFRESH_TOKEN
-});
-
-const drive = google.drive({
-    version: "v3",
-    auth
-});
-
 app.get("/api", (req, res) => {
     res.json({
         success: true,
@@ -36,43 +5,23 @@ app.get("/api", (req, res) => {
     });
 });
 
-app.post("/api/upload", upload.single("file"), async (req, res) => {
-
+app.get("/api/test", async (req, res) => {
     try {
-
-        if (!req.file) {
-            return res.status(400).json({
-                success: false,
-                message: "Tidak ada file."
-            });
-        }
-
-        const stream = Readable.from(req.file.buffer);
-
-        const response = await drive.files.create({
-            requestBody: {
-                name: req.file.originalname
-            },
-            media: {
-                mimeType: req.file.mimetype,
-                body: stream
-            }
+        const about = await drive.about.get({
+            fields: "user"
         });
+
+        res.json(about.data);
+
+    } catch (e) {
 
         res.json({
-            success: true,
-            fileId: response.data.id
-        });
-
-    } catch (err) {
-
-        res.status(500).json({
-            success: false,
-            error: err.message
+            error: e.message
         });
 
     }
-
 });
 
-module.exports = app;
+app.post("/api/upload", upload.single("file"), async (req, res) => {
+    ...
+});
